85dfbbdbd1f61f5f359550c7c43d52b0c0d36fd1
Bruna de Oliveira <bruna.oliveira@workdb.com.br>
2022-03-28 14:30:14 +0200

    Início do desenvolvimento do projeto

--- hello/hello.go
+++ hello/hello.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Hello World - Olá Mundo")
+	nome := "Bruna"
+	versao := 1.1
+
+	fmt.Println("Olá,", nome)
+	fmt.Println("Este programa está na versão:", versao)
+
+	fmt.Println("1 - Iniciar monitoramento")
+	fmt.Println("2 - Exibir logs")
+	fmt.Println("0 - Sair")
+
+	var comando int
+	fmt.Scan(&comando)
+	fmt.Println("Comando informado: ", comando)
+	fmt.Println("Endereço da variável comando:", &comando)
 }
