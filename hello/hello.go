701c5f6966c836ec1952e074de4c47cee99b984a
Bruna de Oliveira <bruna.oliveira@workdb.com.br>
2022-03-28 22:12:34 +0200

    Implementação de métodos com um, vários ou sem retorno e Switch

--- hello/hello.go
+++ hello/hello.go
@@ -2,21 +2,64 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"os"
 )
 
 func main() {
+	exibeIntroducao()
+
+	for {
+		exibeMenu()
+
+		comando := leComando()
+
+		switch comando {
+		case 1:
+			iniciarMonitoramento()
+		case 2:
+			fmt.Println("Exibindo logs...")
+		case 0:
+			fmt.Println("Saindo...")
+			os.Exit(0)
+		default:
+			fmt.Println("Não conheço este comando.")
+			os.Exit(-1)
+		}
+
+	}
+}
+
+func exibeIntroducao() {
 	nome := "Bruna"
 	versao := 1.1
 
 	fmt.Println("Olá,", nome)
 	fmt.Println("Este programa está na versão:", versao)
+}
 
+func exibeMenu() {
 	fmt.Println("1 - Iniciar monitoramento")
 	fmt.Println("2 - Exibir logs")
 	fmt.Println("0 - Sair")
+}
 
-	var comando int
-	fmt.Scan(&comando)
-	fmt.Println("Comando informado: ", comando)
-	fmt.Println("Endereço da variável comando:", &comando)
+func leComando() int {
+	var comandoLido int
+	fmt.Scan(&comandoLido)
+	fmt.Println("Comando informado: ", comandoLido)
+
+	return comandoLido
+}
+
+func iniciarMonitoramento() {
+	fmt.Println("Monitorando...")
+	site := "https://random-status-code.herokuapp.com/"
+	resp, _ := http.Get(site)
+
+	if resp.StatusCode == 200 {
+		fmt.Println("Site:", site, " foi carregado com sucesso!")
+	} else {
+		fmt.Println("Site:", site, " está com problemas. Status code:", resp.StatusCode)
+	}
 }
