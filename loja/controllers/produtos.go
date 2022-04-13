82b323fd5df1c3933d996aad95a1ba33850c5502
Bruna de Oliveira <bruna.oliveira@workdb.com.br>
2022-04-13 13:08:30 +0200

    Go: fundamentos de uma aplicação web - Conclusão do curso

--- loja/controllers/produtos.go
+++ loja/controllers/produtos.go
@@ -43,3 +43,46 @@ func Insert(w http.ResponseWriter, r *http.Request) {
 
 	http.Redirect(w, r, "/", 301)
 }
+
+func Delete(w http.ResponseWriter, r *http.Request) {
+	id := r.URL.Query().Get("id")
+
+	models.Delete(id)
+	http.Redirect(w, r, "/", 301)
+}
+
+func Edit(w http.ResponseWriter, r *http.Request) {
+	id := r.URL.Query().Get("id")
+	produto := models.Edit(id)
+
+	temp.ExecuteTemplate(w, "Edit", produto)
+}
+
+func Update(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		id := r.FormValue("id")
+		nome := r.FormValue("nome")
+		descricao := r.FormValue("descricao")
+		preco := r.FormValue("preco")
+		quantidade := r.FormValue("quantidade")
+
+		idConvertido, err := strconv.Atoi(id)
+		if err != nil {
+			log.Println("Erro na conversão do ID para int:", err)
+		}
+
+		precoConvertido, err := strconv.ParseFloat(preco, 64)
+		if err != nil {
+			log.Println("Erro na conversão do preço para float64:", err)
+		}
+
+		quantidadeConvertida, err := strconv.Atoi(quantidade)
+		if err != nil {
+			log.Println("Erro na conversão da quantidade para int:", err)
+		}
+
+		models.Update(idConvertido, quantidadeConvertida, nome, descricao, precoConvertido)
+	}
+
+	http.Redirect(w, r, "/", 301)
+}
